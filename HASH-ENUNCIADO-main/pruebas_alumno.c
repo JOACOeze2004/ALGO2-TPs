507725f796b27dfa373dc88d5c86d117a2e8bf61
Joaquin Mendez  archerrizo <164963376+JOACOeze2004@users.noreply.github.com>
2024-11-05 17:48:33 -0300

    Agrego insertar, destruir todo y mas pruebas

--- HASH-ENUNCIADO-main/pruebas_alumno.c
+++ HASH-ENUNCIADO-main/pruebas_alumno.c
@@ -1,6 +1,15 @@
 #include "pa2m.h"
 #include "src/hash.h"
+#include <stdlib.h>
+#include <string.h>
 
+struct pokemon {
+	char *nombre;
+	char tipo;
+	int fuerza;
+	int destreza;
+	int resistencia;
+};
 
 void CrearHashRetornaAlgoNoNULL()
 {
@@ -31,6 +40,58 @@ void LaCantidadDeElementosInicialesEnhashNulaEs0()
 		     "La cantidad inicial de un hash NULL es 0");
 	hash_destruir(hash);
 }
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//pruebas de insertar
+void InsertarEnHAshNULLDevuelveFalse()
+{
+	hash_t *hash = hash_crear(2);
+	char *clave = "noc";
+	void *encontrado = NULL;
+
+	pa2m_afirmar(!hash_insertar(NULL,clave,NULL,&encontrado),
+		     "insertar en un hash NULL devuelve false");
+	hash_destruir(hash);
+}
+
+void InsertarConClaveNULLDevuelveFalse()
+{
+	hash_t *hash = hash_crear(2);
+	char *clave = NULL;
+	void *encontrado = NULL;
+
+	pa2m_afirmar(!hash_insertar(hash,clave,NULL,&encontrado),
+		     "insertar con una clave NULL devuelve false");
+	hash_destruir(hash);
+}
+
+
+void InsertarBienDevuelveTrue()
+{
+	hash_t *hash = hash_crear(2);
+
+	struct pokemon pikachu = { "Pikachu", 'E', 55, 40, 50 };
+	char *clave = pikachu.nombre;
+	void *valor = &(pikachu.fuerza);
+	void *encontrado = NULL;
+	pa2m_afirmar(hash_insertar(hash,clave,valor,&encontrado),
+		     "insertamos correctamente un elemento al diccionario");
+	pa2m_afirmar(hash_cantidad(hash) == 1,
+		     "La cantidad aumento correctemente en 1"); 			 
+	pa2m_afirmar( hash_buscar(hash,clave) == valor,
+		     "Encontre la clave porque es distinto de NULL");	 
+	hash_destruir(hash);
+}
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//pruebas de buscar
+void BuscarenHashVAcioDevuelveNULL()
+{
+	hash_t *hash = hash_crear(2);
+	char *clave = "noc";
+	pa2m_afirmar(hash_buscar(hash,clave) == NULL, "buscar en un hash vacio devuelve NULL");
+	hash_destruir(hash);
+}
+
 int main()
 {
 	pa2m_nuevo_grupo("Pruebas de crear Hash");
@@ -39,7 +100,13 @@ int main()
 	LaCantidadDeElementosInicialesEnhashNoNulaEs0();
 	LaCantidadDeElementosInicialesEnhashNulaEs0();
 	
-	pa2m_nuevo_grupo("Pruebas de insertar Hash");
+	pa2m_nuevo_grupo("Pruebas de insertar en Hash");
+	InsertarEnHAshNULLDevuelveFalse();
+	InsertarConClaveNULLDevuelveFalse();
+	InsertarBienDevuelveTrue();
+
+	pa2m_nuevo_grupo("Pruebas de buscar en Hash");
+	BuscarenHashVAcioDevuelveNULL();	
 
 
 	return pa2m_mostrar_reporte();
--- HASH-ENUNCIADO-main/src/hash.c
+++ HASH-ENUNCIADO-main/src/hash.c
@@ -1,13 +1,18 @@
 #include "hash.h"
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 #define FACTOR_CARGA_MAX 0.75
+#define CAPACIDAD_BORDE 3
+
+typedef struct par par_t;
 
 struct par{
     char *clave;
     void* valor;
+    par_t *siguiente;
 };
-typedef struct par par_t;
 
 struct hash{
     par_t **pares;
@@ -15,21 +20,47 @@ struct hash{
     size_t cantidad;
 };
 
-par_t **crear_par(hash_t *hash,size_t cap_inicial_max){
-    hash->pares = calloc(1,sizeof(par_t));
-    hash->capacidad = cap_inicial_max;
-    return hash->pares;
+/*
+Reeveer despues funcion hash
+*/
+
+size_t funcion_hash(const char *clave, size_t capacidad) {
+    size_t hash_posicion = 0;
+    size_t i = 0;
+    while (clave[i] != '\0') {
+        hash_posicion = (hash_posicion * 31 + (unsigned char)clave[i]) % capacidad;
+        i++;
+    }
+    return hash_posicion; 
+}
+
+
+par_t *crear_nuevo_par(char* clave,void *contenido)
+{
+	par_t *nodo = calloc(1,sizeof(par_t));
+	if (nodo == NULL) {
+		return NULL;
+	}
+    nodo->clave = calloc(1,strlen(clave)+1);
+    if (nodo->clave == NULL){
+        free(nodo);
+        return NULL;
+    }
+    strcpy(nodo->clave,clave);
+    nodo->valor = contenido;
+	return nodo;
 }
 
 hash_t* hash_crear(size_t capacidad_inicial)
 {
-    size_t cap_inicial_max = capacidad_inicial < 5 ? 5 : capacidad_inicial;
+    size_t cap_inicial_max = capacidad_inicial < CAPACIDAD_BORDE ? CAPACIDAD_BORDE : capacidad_inicial;
     hash_t *hash =  calloc(1,sizeof(hash_t));
     if (hash == NULL){
         return NULL;
     }
-    par_t **par = crear_par(hash,cap_inicial_max);
-    if (par == NULL)
+    hash->pares = calloc(cap_inicial_max,sizeof(par_t));
+    hash->capacidad = cap_inicial_max;
+    if (hash->pares == NULL)
     {
         free(hash);
         return NULL;
@@ -56,17 +87,69 @@ size_t hash_cantidad(hash_t* hash){
  *
  * Devuelve true si se pudo almacenar el valor.
  **/
-//bool hash_insertar(hash_t* hash, char* clave, void* valor, void** encontrado);
+bool hash_insertar(hash_t* hash, char* clave, void* valor, void** encontrado){
+    if (hash == NULL || clave == NULL ){
+        return false;
+    }
+    if (encontrado != NULL){
+        *encontrado = NULL;
+    }
+    if ((float)hash->cantidad / (float)hash->capacidad >= FACTOR_CARGA_MAX)
+    {
+        //hash = rehash(hash);
+    }
+    size_t indice_hash = funcion_hash(clave,hash->capacidad);
+    par_t *pos_actual = hash->pares[indice_hash];
+    bool clave_repetida_insertada = false;
+    while (pos_actual != NULL && !clave_repetida_insertada){
+        if (strcmp(clave,pos_actual->clave) == 0){
+            if (encontrado != NULL){
+                *encontrado = pos_actual->valor;
+            }
+            pos_actual->valor = valor;
+            clave_repetida_insertada = true;
+        }
+        pos_actual = pos_actual->siguiente;        
+    }
+    
+    if (!clave_repetida_insertada){
+        par_t *par_nuevo = crear_nuevo_par(clave,valor);
+        if (par_nuevo == NULL){
+            return false;
+        }
+        par_nuevo->siguiente = hash->pares[indice_hash];
+        hash->pares[indice_hash] = par_nuevo;
+        hash->cantidad++;
+    } 
+    return true;
+}
 
-/**
- * Busca el elemento asociado a la clave en la tabla.
- **/
-//void* hash_buscar(hash_t* hash, char* clave);
+par_t *dict_buscar_par(hash_t *hash,char *clave){ 
+    size_t indice = funcion_hash(clave,hash->capacidad);
+    par_t *par_actual  =  hash->pares[indice];
+    bool clave_encontrada = false;
+    void* elemento = NULL;
+    while (par_actual != NULL && !clave_encontrada){
+        if (strcmp(clave,par_actual->clave)== 0){
+            elemento = par_actual;
+            clave_encontrada = true;
+        }
+        par_actual = par_actual->siguiente;      
+    }
+    return elemento;
+}
 
-/**
- *Devuelve si la clave existe en la tabla o no
- */
-//bool hash_contiene(hash_t* hash, char* clave);
+void* hash_buscar(hash_t* hash, char* clave){
+    if (hash == NULL || clave == NULL || hash->cantidad == 0){
+        return NULL;
+    }
+    par_t *par = dict_buscar_par(hash,clave);
+    return par ? par->valor : NULL; 
+}
+
+bool hash_contiene(hash_t* hash, char* clave){
+    return (hash_buscar(hash,clave) != NULL || hash != NULL || clave != NULL) ? true : false;
+}
 
 /**
  * Quita el elemento asociado a la clave de la tabla y lo devuelve.
@@ -85,11 +168,16 @@ size_t hash_cantidad(hash_t* hash){
 void destruir_todo(hash_t *hash,  void (*destructor)(void *)){
     if (hash != NULL)
     {
-        if (destructor != NULL)
-        {
-            for (size_t i = 0; i < hash->capacidad; i++){
-                par_t *par_actual = hash->pares[i];
-                destructor(par_actual);   
+        for (size_t i = 0; i < hash->capacidad; i++){
+            par_t *par_actual = hash->pares[i];
+            while (par_actual != NULL){
+                par_t *par_a_eliminar = par_actual;
+                par_actual = par_actual->siguiente;
+                if (destructor != NULL){
+                    destructor(par_a_eliminar);  
+                }
+                free(par_a_eliminar->clave);
+                free(par_a_eliminar);
             }
         }
         free(hash->pares);
@@ -102,6 +190,7 @@ void hash_destruir(hash_t* hash){
         destruir_todo(hash,NULL);
     }
 }
+
 void hash_destruir_todo(hash_t* hash, void (*destructor)(void*)){
     if (hash != NULL && destructor != NULL){
         destruir_todo(hash,destructor);
