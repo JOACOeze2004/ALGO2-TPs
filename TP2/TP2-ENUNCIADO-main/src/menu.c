bc68ab0a3b6120467b153d7b7929887bcbeee48d
Joaquin Mendez  archerrizo <164963376+JOACOeze2004@users.noreply.github.com>
2024-11-17 20:23:22 -0300

    Pequeños cambios al TDA Menu

--- TP2/TP2-ENUNCIADO-main/src/menu.c
+++ TP2/TP2-ENUNCIADO-main/src/menu.c
@@ -83,7 +83,7 @@ bool menu_eliminar_opcion(menu_t *menu, char opcion){
 	}
 	char clave[2] = {opcion, '\0'};
 	opcion_t *opcion_eliminar = hash_quitar(menu->almacen,clave);
-	if (opcion_eliminar == NULL){
+	if (!opcion_eliminar){
 		return false;
 	}
 	acomodar_opciones(menu,opcion_eliminar);
@@ -98,7 +98,7 @@ void (*menu_obtener_funcion_opcion(menu_t *menu, char opcion))(void){
 	}
 	char clave[2] = {opcion, '\0'};
 	opcion_t *opcion_buscada = hash_buscar(menu->almacen,clave);
-	if (opcion_buscada == NULL){
+	if (!opcion_buscada){
 		return NULL;
 	}
 	return opcion_buscada->funcion;
@@ -115,3 +115,57 @@ void menu_destruir(menu_t *menu){
 	}	
 }
 
+/*
+
+menu_t *menu_crear() {
+    menu_t *menu = calloc(1, sizeof(menu_t));
+    if (!menu) {
+        return NULL;
+    }
+    menu->almacen = hash_crear(CAPACIDAD_INICIAL);
+    if (!menu->almacen) {
+        free(menu);
+        return NULL;
+    }
+    return menu;
+}
+
+bool menu_agregar_opciones(menu_t *menu, char opcion, void (*funcion)(void)) {
+    if (!menu || !funcion) {
+        return false;
+    }
+    opcion_t *nueva_opcion = calloc(1, sizeof(opcion_t));
+    if (!nueva_opcion) {
+        return false;
+    }
+    nueva_opcion->funcion = funcion;
+
+    char clave[2] = {opcion, '\0'};
+    if (!hash_insertar(menu->almacen, clave, nueva_opcion, NULL)) {
+        free(nueva_opcion);
+        return false;
+    }
+    return true;
+}
+
+
+bool menu_eliminar_opcion(menu_t *menu, char opcion) {
+    if (!menu) {
+        return false;
+    }
+    char clave[2] = {opcion, '\0'};
+    opcion_t *opcion_eliminada = hash_quitar(menu->almacen, clave);
+    if (!opcion_eliminada) {
+        return false;
+    }
+    free(opcion_eliminada);
+    return true;
+}
+
+void menu_destruir(menu_t *menu) {
+    if (menu) {
+        hash_destruir_todo(menu->almacen, free);
+        free(menu);
+    }
+}
+*/
