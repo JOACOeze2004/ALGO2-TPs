7e732a0b9cc7440a0ead899dd0e7cfde3d79d087
Joaquin Mendez  archerrizo <164963376+JOACOeze2004@users.noreply.github.com>
2024-11-20 21:52:23 -0300

    Version con menu basico, falta ponele las funciones

--- TP2/TP2-ENUNCIADO-main/tp2.c
+++ TP2/TP2-ENUNCIADO-main/tp2.c
@@ -1,6 +1,10 @@
 #include "extra/engine.h"
 #include "extra/ansi.h"
 #include <stdio.h>
+#include "src/menu.h"
+#include "src/pokedex.h"
+
+
 
 struct jugador {
 	int x;
@@ -60,13 +64,91 @@ int logica(int entrada, void *datos)
 	return entrada == 'q' || entrada == 'Q';
 }
 
+
+void mostrar_pokedex(){
+	printf("Aqui estan los pokemones que se ingresaron... \n");
+}
+
+void avanzar_juego(){
+	printf("Inicializando el juego... \n");
+}
+
+void elegir_semilla(){
+	printf("deberias ingresar una semilla... \n");
+}
+
+void cerrar_juego(){
+	printf("Saliendo del juego...\n");
+	return;
+}
+
+
+//post: Imprime una pequeña intro para que el usuario sepa que comandos pasarle y como "reaccionan" al mismo.
+void imprimir_inicio()
+{
+	printf(ANSI_COLOR_BLUE"\t\t\t\t\t\t\t-----------------------------------------------------\n"); 
+    printf("\t\t\t\t\t\t\t|" ANSI_COLOR_GREEN ANSI_COLOR_BOLD"    BIENVENIDO A POKEMON GO 2: AHORA ES PERSONAL"ANSI_COLOR_BLUE "   |\n");
+    printf(ANSI_COLOR_BLUE"\t\t\t\t\t\t\t-----------------------------------------------------\n"ANSI_COLOR_RESET);
+    printf("\n");
+
+	printf(ANSI_COLOR_MAGENTA"Hola jugador,bienvenido a " ANSI_COLOR_BOLD "Pokemon go 2\n"ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_GREEN ANSI_COLOR_BOLD"El objetivo del juego es que atrapes pokemones, antes de que se acabe el tiempo.\n"ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_GREEN"Lo ideal es que trapes a pokemones con el mismo color, asi obtendras un multiplicador al puntaje al capturarlos.\n");
+	printf(ANSI_COLOR_GREEN"Los pokemones se mueven a lo largo del tablero, pero no se mueven si no te moves.\n");
+	printf(ANSI_COLOR_GREEN"Por ultimo tenes 60 segundos de juego en total, una vez llegado a 0, finaliza el juego.\n");
+	printf(ANSI_COLOR_RED ANSI_COLOR_BOLD"A continuacion veras un menu en el cual podras elegir entre 4 opciones.\n");
+	printf(ANSI_COLOR_RED "Cabe aclarar que tambien se aceptan las minusculas\n"ANSI_COLOR_RESET);
+
+	printf(ANSI_COLOR_YELLOW"Una vez sabida esta informacion, ya puedes empezar a jugar, suerte!\n"ANSI_COLOR_RESET);
+	
+	printf(ANSI_COLOR_RED"====================================================\n"); 
+	printf(ANSI_COLOR_CYAN"| [J]: Empezar Juego\n"ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_CYAN"| [S]: Empezar Juego usando una semilla que eligas\n"ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_CYAN"| [P]: Mostrar todos los pokemones en la pokedex\n"ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_CYAN"| [Q]: Salir del juego\n"ANSI_COLOR_RESET);
+	printf(ANSI_COLOR_RED"===================================================\n"ANSI_COLOR_RESET);
+}
+
+
 int main()
 {
-	struct jugador jugador = { 0 };
+	imprimir_inicio();
 
-	game_loop(logica, &jugador);
 
-	mostrar_cursor();
+	menu_t *menu = menu_crear();
+	menu_agregar_opciones(menu,'P',mostrar_pokedex);
+	menu_agregar_opciones(menu,'p',mostrar_pokedex);
 
+	menu_agregar_opciones(menu,'J',avanzar_juego);
+	menu_agregar_opciones(menu,'j',avanzar_juego);
+
+	menu_agregar_opciones(menu,'S',elegir_semilla);
+	menu_agregar_opciones(menu,'s',elegir_semilla);
+
+	menu_agregar_opciones(menu,'Q',cerrar_juego);
+	menu_agregar_opciones(menu,'q',cerrar_juego);
+
+	char entrada;
+	int opcion = -1;
+	while (opcion == -1) {
+		printf("\nIngrese la opcion que quiera ------------> ");
+		if (scanf(" %c", &entrada) == 1) {
+			if (!menu_ejecutar_entrada(menu, entrada)) {
+                printf("\nERROR 405, por favor ingrese una opcion correcta");
+            } else {
+                opcion = 0; 
+            }
+		}
+	}	
+
+
+	//struct jugador jugador = { 0 };
+
+	//game_loop(logica, &jugador);
+
+	//mostrar_cursor();
+
+
+	menu_destruir(menu);
 	return 0;
 }
